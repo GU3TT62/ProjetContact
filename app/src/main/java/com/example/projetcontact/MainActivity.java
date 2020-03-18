a32c321a6136fe672f9619aa18268410a63241ec
GU3TT <duplouy.gaetan@laposte.net>
2020-03-18 16:47:39 +0100

    Merge branch 'master' of C:\Users\gaeta\Desktop\Java\API\ProjetContact with conflicts.

--- app/src/main/java/com/example/projetcontact/MainActivity.java
+++ app/src/main/java/com/example/projetcontact/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.projetcontact;
-
+//Ceci est un test
 import android.app.SearchManager;
 import android.content.Intent;
 import android.database.Cursor;
@@ -89,35 +89,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatic private ally handle clicks on the Home/Up button, so long
+        // automaticprivate ally handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
-        }*/
+        }
 
         return super.onOptionsItemSelected(item);
     }
     private void fillData() {
         // Get all of the notes from the database and create the item list
         Cursor c = Db.fetchAllContact();
-        startManagingCursor(c);
-
-        String[] from = new String[] { DbContact.KEY_NOM,DbContact.KEY_PRENOM};
-        int[] to = new int[] { R.id.champ1,R.id.champ2 };
-
-        // Now create an array adapter and set it to display using our row
-
-        SimpleCursorAdapter contact = new SimpleCursorAdapter(this, R.layout.contact, c, from, to);
-        listView.setAdapter(contact);
-
-    }
-
-    private void fillDataFav() {
-        // Get all of the notes from the database and create the item list
-        Cursor c = Db.fetchFavs();
         startManagingCursor(c);
 
         String[] from = new String[] { DbContact.KEY_NOM,DbContact.KEY_PRENOM};
