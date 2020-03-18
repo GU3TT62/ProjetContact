6fb6e3e2e2114eecf3435d6e89485d2dbfd935c9
GU3TT <duplouy.gaetan@laposte.net>
2020-03-18 14:11:07 +0100

    projet

---
+++ app/src/main/java/com/example/projetcontact/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.projetcontact;
+
+import android.app.SearchManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.ActionMode;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private DbContact Db;
+    private int nbTache=1;
+    private ActionMode actionMode;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+        listView = (ListView) findViewById(R.id.ListContact);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (actionMode != null) {
+                    return false;
+                }
+
+                actionMode = MainActivity.this.startActionMode(ActionModeCallback);
+                actionMode.setTag(position);
+
+
+
+                return true;
+            }
+
+
+        });
+
+
+        FloatingActionButton fab = findViewById(R.id.add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Db.createContact("truc", "truc test de contact","trg","7845",false);
+                fillData();
+
+            }
+        });
+        Db=new DbContact(this);
+        Db.open();
+        fillData();
+    }
+    public void add(View view) {
+
+        Db.createContact("Tâche", "truc test de contact","trg","7845",false);
+        fillData();
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automaticprivate ally handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void fillData() {
+        // Get all of the notes from the database and create the item list
+        Cursor c = Db.fetchAllContact();
+        startManagingCursor(c);
+
+        String[] from = new String[] { DbContact.KEY_NOM,DbContact.KEY_PRENOM};
+        int[] to = new int[] { R.id.champ1,R.id.champ2 };
+
+        // Now create an array adapter and set it to display using our row
+
+        SimpleCursorAdapter contact = new SimpleCursorAdapter(this, R.layout.contact, c, from, to);
+        listView.setAdapter(contact);
+
+    }
+
+
+
+
+
+
+
+
+
+
+    private ActionMode.Callback ActionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.context_menu, menu);
+            mode.setTitle("Choix Action");
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            int pos = Integer.parseInt(mode.getTag().toString());
+            Cursor SelectedTaskCursor = (Cursor) listView.getItemAtPosition(pos);
+            final String SelectedTask = SelectedTaskCursor.getString(SelectedTaskCursor.getColumnIndex("nom"));
+            switch (item.getItemId()) {
+                case R.id.supp:
+                    //Db.deleteContact(pos);
+                    mode.finish();
+                    return true;
+
+
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            actionMode = null;
+        }
+    };
+
+
+
+
+
+
+
+
+
+}
